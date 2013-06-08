0111c8e1d3a0075060d5a8e0501a20f2344a0ff7
Boohbah <boohbah@gmail.com>
2013-06-08 07:10:53 +0000

    Add GPLv2

--- raur.rb
+++ raur.rb
@@ -1,8 +1,25 @@
 #!/usr/bin/env ruby
+#
 # raur
+#
 # A simple AUR helper in Ruby
-# by Boohbah <boohbah at gmail.com>
 # Depends on pacman, makepkg, sudo, and the minitar gem
+#
+# Copyright (c) 2013 Boohbah <boohbah at gmail.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
 
 aurdir = "/home/user/aur" # Change this
 
@@ -76,7 +93,6 @@ rescue
   puts error + $!.to_s + plain
   exit
 end
-
 
 # Extract
 begin
