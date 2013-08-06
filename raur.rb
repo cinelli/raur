14f76c6368e6978f41b596fd713c9664c903d606
Boohbah <boohbah@gmail.com>
2013-08-06 01:16:39 +0000

    Remove aur_json.rb

--- raur.rb
+++ raur.rb
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-aurdir = "/hhome/user/aur" # Change this
+aurdir = "/home/user/aur" # Change this
 
 require 'archive/tar/minitar'
 require 'io/console'
