d8d75061094d5ca541d0ddd03f4495bf2738efbb
Boohbah <boohbah@gmail.com>
2013-09-21 08:00:39 +0000

    Remove minitar

--- raur.rb
+++ raur.rb
@@ -2,7 +2,7 @@
 #
 # raur
 # A simple AUR helper in Ruby
-# Depends on pacman, makepkg, sudo, and the minitar gem
+# Depends on pacman, makepkg, sudo
 #
 # Copyright (c) 2013 Boohbah <boohbah at gmail.com>
 # 
@@ -21,93 +21,78 @@
 
 aurdir = "/home/user/aur" # Change this
 
-require 'archive/tar/minitar'
 require 'io/console'
 require 'open-uri'
+require File.expand_path('../untar.rb', __FILE__)
 
-# Colors
-RED   = "\e[1;31m"
-GREEN = "\e[0;32m"
-WHITE = "\e[1;37m"
-RC = "\e[0m"
+pkg = ARGV.first
 
-ERROR = "#{RED}==> ERROR: #{WHITE}"
-INFO  = "#{GREEN}==> #{WHITE}"
-
-def info s
-  print INFO + s + RC
+if pkg.nil?
+  raise "No argument given. Specify the AUR package you want to build.\n" +
+        "Usage: raur pkgname"
 end
 
-begin
-  pkg = ARGV.first
-
-  if pkg.nil?
-    raise "No argument given. Specify the AUR package you want to build.\n" +
-          "#{INFO}Usage: raur pkgname"
+# Check for required executables
+%w(/usr/bin/pacman /usr/bin/makepkg /usr/bin/sudo).each do |file|
+  unless File.executable? file
+    raise "#{file} does not exist or is not executable."
   end
+end
 
-  # Check for required executables
-  %w(/usr/bin/pacman /usr/bin/makepkg /usr/bin/sudo).each do |file|
-    unless File.executable? file
-      raise "#{file} does not exist or is not executable."
-    end
-  end
+pkgdir = "#{aurdir}/#{pkg}"
 
-  #unless File.writable? aurdir
-  #  raise "Directory #{aurdir} does not exist or is not writable."
-  #end
-
-  pkgdir = "#{aurdir}/#{pkg}"
-
-  # Determine if a package directory with this name exists
-  if File.directory? pkgdir
-    info "Remove existing directory #{pkgdir} ? [y/N] "
+# Determine if a package directory with this name exists
+if File.directory? pkgdir
+  print "Remove existing directory #{pkgdir} ? [y/N] "
+  puts input = STDIN.getch
+  case input
+  when 'y', 'Y'
+    puts "Removing #{pkgdir}"
+    FileUtils.rm_rf pkgdir
+  else
+    print "Continue building #{pkg} ? [Y/n] "
     puts input = STDIN.getch
     case input
-    when 'y', 'Y'
-      info "Removing #{pkgdir}\n"
-      FileUtils.rm_rf pkgdir
+    when 'y', 'Y', "\r"
+      puts "Writing over existing #{pkgdir}"
     else
-      info "Continue building #{pkg} ? [Y/n] "
-      puts input = STDIN.getch
-      case input
-      when 'y', 'Y', "\r"
-        info "Writing over existing #{pkgdir}\n"
-      else
-        exit
-      end
+      exit
     end
   end
-
-  url = "https://aur.archlinux.org/packages/#{pkg[0..1]}/#{pkg}/#{pkg}.tar.gz"
-  tarball = "#{aurdir}/#{pkg}.tar.gz"
-
-  # Download tarball
-  File.open(tarball, 'wb') {|f| f.write open(url).read }
-
-  # Extract
-  tgz = Zlib::GzipReader.new(File.open(tarball, 'rb'))
-  Archive::Tar::Minitar.unpack(tgz, aurdir)
-
-  # Build
-  Dir.chdir(pkgdir)
-  raise "makepkg failed." unless system "makepkg -sf"
-
-  # Find newest file
-  pkgfile = Dir.entries(pkgdir).sort_by {|f|
-    File.mtime(File.join(pkgdir,f))
-  }.last
-
-  # Install
-  raise "Failed to install #{pkgfile}" unless system "sudo pacman -U #{pkgfile}"
-
-  # Cleanup
-  File.delete(tarball)
-
-  info "Installed #{pkg}\n"
-
-rescue OpenURI::HTTPError
-  puts "#{ERROR}#{$!}#{RC}\n#{url}"
-rescue
-  puts "#{ERROR}#{$!}#{RC}"
 end
+
+url = "https://aur.archlinux.org/packages/#{pkg[0..1]}/#{pkg}/#{pkg}.tar.gz"
+tarball = "#{aurdir}/#{pkg}.tar.gz"
+
+# Download tarball
+begin
+  File.open(tarball, 'wb') {|f| f.write open(url).read }
+rescue OpenURI::HTTPError
+  puts "#{url}\n#{$!}"
+  exit
+end
+
+# Extract
+tgz = Zlib::GzipReader.new(File.open(tarball, 'rb'))
+untar(tgz, aurdir)
+
+# Build
+Dir.chdir(pkgdir)
+unless system "makepkg -sf"
+  raise "makepkg failed."
+end
+
+# Find newest file
+pkgfile = Dir.entries(pkgdir).sort_by {|f|
+  File.mtime(File.join(pkgdir, f))
+}.last
+
+# Install
+unless system "sudo pacman -U #{pkgfile}"
+  raise "Failed to install #{pkgfile}"
+end
+
+# Cleanup
+File.delete(tarball)
+
+puts "Installed #{pkg}"
