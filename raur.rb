21c9303e04e50c12eb28536eb67009f5052fe315
Boohbah <boohbah@gmail.com>
2013-09-21 08:09:50 +0000

    Inline function

--- raur.rb
+++ raur.rb
@@ -23,7 +23,30 @@ aurdir = "/home/user/aur" # Change this
 
 require 'io/console'
 require 'open-uri'
-require File.expand_path('../untar.rb', __FILE__)
+require 'rubygems/package'
+
+# https://gist.github.com/sinisterchipmunk/1335041
+def untar(io, destination)
+  Gem::Package::TarReader.new io do |tar|
+    tar.each do |tarfile|
+      destination_file = File.join destination, tarfile.full_name
+
+      if tarfile.directory?
+        FileUtils.mkdir_p destination_file
+      else
+        destination_directory = File.dirname(destination_file)
+
+        unless File.directory?(destination_directory)
+          FileUtils.mkdir_p destination_directory
+        end
+
+        File.open destination_file, "wb" do |f|
+          f.print tarfile.read
+        end
+      end
+    end
+  end
+end
 
 pkg = ARGV.first
 
